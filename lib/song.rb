5da82683099914534938f38ac200c0f874c4c1e8
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-13 16:56:39 -0600

    commit tree 5e47954721ef4faf8849b7ec3179e4d4471fd09d

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,53 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.create_by_name(name)
+    song = create
+    song.name = name
+    song
+  end
+
+  def self.new_by_name(name)
+    song = new
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(sname)
+    all.find { |song| song.name == sname }
+  end
+
+  def self.find_or_create_by_name(sname)
+    find_by_name(sname) || create_by_name(sname)
+  end
+
+  def self.alphabetical
+    all.sort_by(&:name)
+  end
+
+  def self.new_from_filename(filename)
+    piece = filename.split(' - ')
+    song = new
+    song.name = piece[1].gsub('.mp3', '')
+    song.artist_name = piece[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    piece = filename.split(' - ')
+    song = create
+    song.name = piece[1].gsub('.mp3', '')
+    song.artist_name = piece[0]
+    song
+  end
+
+  def self.destroy_all
+    all.clear
+  end
 end
